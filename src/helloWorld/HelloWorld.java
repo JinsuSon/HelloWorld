b27fc826f66b609de4778a97fc97a0cc66b19e06
sjs08 <sjs08@DESKTOP-U8QUNSQ>
2022-02-19 21:14:12 +0900

    갈라디아서 추가

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
 	public static void main(String[] arg) {
 		System.out.println("이사야 41:10");
 		System.out.println("너는 두려워 말라, 내가 너와 함께함이라. 놀라지 말라, \n내가 네 하나님임이라. 내가 너를 도우리라. 정녕, 내가 내 의의 오른손으로 너를 높이리라.");
+		System.out.println("갈라디아서 6:9");
+		System.out.println("선을 행하다가 낙심하지 말지니, 쓰러지지 아니하면 때가 되어 거두리라.");
 	}
 }
