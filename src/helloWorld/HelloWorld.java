2f3ebda120000173f3e346c7bccf36f730d8e886
sjs08 <sjs08@DESKTOP-U8QUNSQ>
2022-02-19 20:53:28 +0900

    initial commit

---
+++ src/helloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+package helloWorld;
+
+public class HelloWorld {
+	public static void main(String[] arg) {
+		System.out.println("이사야 41:10");
+		System.out.println("너는 두려워 말라, 내가 너와 함께함이라. 놀라지 말라, \n내가 네 하나님임이라. 내가 너를 도우리라. 정녕, 내가 내 의의 오른손으로 너를 높이리라.");
+	}
+}
